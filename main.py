c014c2f400d9d5d6c26bcc4b286ae17c06288f86
haleysmith8 <72937151+haleysmith8@users.noreply.github.com>
2020-10-22 08:27:26 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+noun1 = input("enter a noun: ")
+noun1 = str(noun1)
+verb1 = input("enter a verb: ")
+verb1 = str(verb1)
+verb2 = input("enter a second verb: ")
+
+print("Hello, my name is " + noun1 + " and I love to " + verb1 + " and " + verb2 + "." )
